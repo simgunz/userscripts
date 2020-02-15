766c0e77fd3d54ae3e1d335dd4e842c13f2c226a
Simone Gaiarin <simgunz@gmail.com>
2020-02-15 12:03:24 +0100

    Fix amazon return labels

--- amazon-return-labels-page-break.js
+++ amazon-return-labels-page-break.js
@@ -1,23 +1,27 @@
 // ==UserScript==
 // @name         Amazon return labels page break
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Put each amazon return label in a separate page. Fix for Amazon Italy.
 // @author       Simone Gaiarin
-// @match        https://www.amazon.*/spr/returns/label/*
+// @match        https://www.amazon.it/spr/returns/label/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $('.return-label-image').css('break-after', 'page');
-    $('head').append('<style type="text/css" media="print" id="print"></style>')
-    $("#print").text(
-        "@media print {" +
-            ".return-label-image{" +
-            "break-after: page" +
-            "}" +
+
+    $('head').append('<style type="text/css" id="label-print"></style>')
+    $("#label-print").text(
+        "@page {" +
+          "size: A4;" +
+          "margin: 0;" +
         "}"
     );
+    $('.printable-section').css('position', 'relative')
+    $('.return-label-image').each( function() {
+        let section = $($(this).parents()[1])
+        section.after('<div style="break-after: page"><hr/></div>')
+    });
 })();
